7682530ea2f01af31fa43c7ce8ec2c31c57b3ebe
ranaharshsingh <harshsinghrajput1102@gmail.com>
2026-07-26 23:42:52 +0530

    creating wIP: profile card progress

---
+++ storage/theme-convertor/script.js
@@ -0,0 +1,37 @@
+const users = [
+  {
+    name: "amisha rathore",
+    pic: "https://i.pinimg.com/736x/cd/9b/1c/cd9b1cf5b96e8300751f952488d6c002.jpg",
+    bio: "silent chaos in a loud world  | not for everyone"
+  },
+  {
+    name: "kiara mehta",
+    pic: "https://i.pinimg.com/736x/1f/2f/85/1f2f856bf3a020ed8ee9ecb3306ae074.jpg",
+    bio: "main character energy | coffee > everything "
+  },
+  {
+    name: "isha oberoi",
+    pic: "https://i.pinimg.com/736x/23/48/7e/23487ef1268cfe017047a0640318c0d0.jpg",
+    bio: "walking through dreams in doc martens  | late night thinker"
+  },
+  {
+    name: "ojin oklawa",
+    pic: "https://i.pinimg.com/736x/01/be/94/01be94b0b5bf03a50b5d6c4bfec78063.jpg",
+    bio: "too glam to give a damn  | filter free soul"
+  },
+  {
+    name: "riya sharma",
+    pic: "https://i.pinimg.com/736x/2b/f6/2b/2bf62b1d93f8c51f5e61d60b7683d14b.jpg",
+    bio: "creating memories one sunset at a time "
+  },
+  {
+    name: "aanya singh",
+    pic: "https://i.pinimg.com/736x/6a/85/64/6a8564b648bb4dd4d1fdd7d8ab90e2b2.jpg",
+    bio: "books, beaches & good vibes "
+  },
+  {
+    name: "meera kapoor",
+    pic: "https://i.pinimg.com/736x/5d/33/9b/5d339b22cb7c1f2b5f54c6d0d0fca5aa.jpg",
+    bio: "living softly, dreaming loudly "
+  }
+];
